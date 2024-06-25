0f70e7d357b35083b2f6f7b796ebfa98c85c78b9
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2024-06-25 21:51:13 +0530

    ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -10,3 +10,27 @@ function reverse() {
 }
 
 console.log(reverse());
+
+// second approach
+
+function ReverseString() {
+    let str = prompt("Enter string:");
+   return str.split('').reverse().join('');
+}
+
+console.log(ReverseString());
+
+// third approach 
+
+function ReverseString() {
+    let str = prompt("Enter string");
+    let backward = [];
+    let total = str.length - 1;
+    for (let i = total; i >= 0; i--) {
+        backward.push(str[i]);
+    }
+    return backward.join('');
+}
+
+console.log(ReverseString());
+
